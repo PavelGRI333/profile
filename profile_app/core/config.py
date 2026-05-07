1a67ce2507107e49a066c9ec7f7a5fba22f5343e
Pasha <gr.pavel.20011@gmail.com>
2026-05-07 15:50:04 +0300

    Config read from env

--- profile_app/core/config.py
+++ profile_app/core/config.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel
-from pydantic_settings import BaseSettings
 from pydantic import PostgresDsn
 
+from pydantic_settings import (
+    BaseSettings,
+    SettingsConfigDict,
+)
 
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
@@ -21,6 +24,12 @@ class DatabaseConfig(BaseModel):
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
